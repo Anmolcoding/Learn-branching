dcbe5803b2e086bc4ab1de22fc47cdc1552ee4fe
Anmolcoding <panmol607@gmail.com>
2024-09-27 14:13:34 +0530

    This is my first v1

---
+++ calculator.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Read two numbers from the user
+read -p "Enter first number: " a
+read -p "Enter second number: " b
+
+# Perform the addition
+x=$((a + b))
+
+# Print the result
+echo "The sum is: $x"
+
+
+# End
+~       
